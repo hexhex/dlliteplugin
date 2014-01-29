9e79133b8fbb4ea54f3bacaa2a1cbfcac80e7c9d
Christoph Redl <redl@kr.tuwien.ac.at>
2014-01-29 13:29:31 +0100

    prepare script for benchmarking with drew

--- benchmarks/LUBM/benchmark_single.sh
+++ benchmarks/LUBM/benchmark_single.sh
@@ -7,7 +7,8 @@ export LD_LIBRARY_PATH=$2
 instance=$3
 to=$4
 
-confstr=";--supportsets;-n=1;--supportsets -n=1"
+owlfile="${instance%%.hex}.owl"
+confstr="dlvhex2 --heuristics=monolithic --plugindir=../../../src/ --ontology=$owlfile $instance;dlvhex2 --heuristics=monolithic --plugindir=../../../src/ --ontology=$owlfile $instance --supportsets;../calldrew.sh $instance $owlfile;dlvhex2 --heuristics=monolithic --plugindir=../../../src/ --ontology=$owlfile $instance -n=1;dlvhex2 --heuristics=monolithic --plugindir=../../../src/ --ontology=$owlfile $instance --supportsets -n=1"
 confstr2=$(cat conf)
 if [ $? == 0 ]; then
         confstr=$confstr2
@@ -32,18 +33,21 @@ i=0
 for c in "${confs[@]}"
 do
 	echo -ne -e " "
-	output=$(timeout $to time -o $instance.$i.time.dat -f %e dlvhex2 $c --heuristics=monolithic --plugindir=../../../src/ $instance --verbose=8 2>$instance.$i.verbose.dat > /dev/null)
+	output=$(timeout $to time -o $instance.$i.time.dat -f %e $c > /dev/null 2> $instance.$i.verbose.dat)
 	ret=$?
+
+	# drew does not notify the caller about errors, therefore we need this hack
+	err=$(cat $instance.$i.verbose.dat | grep "Exception" | grep "java" | wc -l)
+	if [[ $err -ge 1 ]]; then
+		ret=1
+	fi
+
 	if [[ $ret == 0 ]]; then
 	        output=$(cat $instance.$i.time.dat)
-		groundertime=$(cat $instance.$i.verbose.dat | grep -a "HEX grounder time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
-	        solvertime=$(cat $instance.$i.verbose.dat | grep -a "HEX solver time:" | tail -n 1 | grep -P -o '[0-9]+\.[0-9]+s' | sed "s/s//")
 	else
 		output="---"
-		groundertime="---"
-		solvertime="---"
 	fi
-	echo -ne "$output $groundertime $solvertime"
+	echo -ne "$output"
 
 	rm $instance.$i.time.dat
 	rm $instance.$i.verbose.dat
