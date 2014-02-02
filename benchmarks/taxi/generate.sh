382ad7c2cac36b7a1ac727f90f6675709982bbb9
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-02 14:25:53 +0100

    update benchmark generator

--- benchmarks/taxi/generate.sh
+++ benchmarks/taxi/generate.sh
@@ -1,31 +1,40 @@
 # $1: number of drivers
 # $2: number of regions
 # $3: number of customers
-# $4: edge propability
+# $4: customer probability
 
 prop=$((32768 * $4 / 100)) 
 for (( i=1; i <= $3; i++ ))
 do
-	for (( j = 1; j <= $2; j++ ))
-	do
-		if [[ $RANDOM -le $prop ]]; then
-			echo "isIn(\"c$i\",\"r$j\")."
-		fi
-		if [[ $RANDOM -le $prop ]]; then
-			echo "needsTo(\"c$i\",\"r$j\")."
-		fi
-	done
+	if [[ $RANDOM -le $prop ]]; then 
+		r=$((RANDOM%$2+1))
+		echo "needsTo(\"c$i\",\"r$r\")."
+		r=$((RANDOM%$2+1))
+		echo "isIn(\"c$i\",\"r$r\")."
+	fi
 done
+
+if [[ $4 -le 33 ]]; then 
+	prop1=$((32767 * 30 / 100)) 
+
+elif [[ $4 -le 66 ]]; then 
+	prop1=$((32767 * 70 / 100)) 
+
+else 
+	prop1=32767
+fi 
 
 for (( i=1; i <= $1; i++ ))
 do
-	for (( j = 1; j <= $2; j++ ))
-	do
-		if [[ $RANDOM -le $prop ]]; then
-			echo "isIn(\"d$i\",\"r$j\")."
-		fi
-		if [[ $RANDOM -le $prop ]]; then
-			echo "drivesTo(\"d$i\",\"r$j\")."
-		fi
+	if [[ $RANDOM -le $prop1 ]]; then
+		r=$((RANDOM%$2+1))
+		echo "isIn(\"d$i\",\"r$r\")."
+	fi		
+	
+	for ((j=1; j<=$2; j++))
+		do
+			if [[ $RANDOM -le $prop1 ]]; then
+				echo "goTo(\"d$i\",\"r$j\")."	
+			fi	
 	done
-done
+done	
