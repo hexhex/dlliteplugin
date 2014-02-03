a95b50aa786d63e2dd2cce2f8419f607b72ea698
Daria Stepanova <dasha@kr.tuwien.ac.at>
2014-02-03 14:06:25 +0100

    Test generation files and big ontology are added for generating large taxi instances

---
+++ benchmarks/taxi/generateset_big.sh
@@ -0,0 +1,23 @@
+if [[ $# -lt 4 ]]; then
+	echo "Error: Script expects 4 parameters"
+	exit 1;
+fi
+
+for (( prop=$1; prop <= $2; prop+=$3 ))
+do
+	for (( inst=0; inst < $4; inst++ ))
+	do
+		propf=`printf "%03d" ${prop}`
+		in=`printf "%03d" ${inst}`
+
+		# create ontology instances
+		cp ontology_big.owl instances/inst_size_${propf}_inst_${in}.owl
+
+		# instantiate the program
+		./generate_big.sh 200 5 500 $prop > "instances/inst_size_${propf}_inst_${in}.dlp"
+		cp instances/inst_size_${propf}_inst_${in}.dlp instances/inst_size_${propf}_inst_${in}.hex
+
+		cat program.hex | sed "s/OWLONTOLOGY/\"inst_size_${propf}_inst_${in}.owl\"/g" >> "instances/inst_size_${propf}_inst_${in}.hex"
+		cat program.dlp >> "instances/inst_size_${propf}_inst_${in}.dlp"
+	done
+done
