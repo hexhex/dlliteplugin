f8cf1b20fd946087772d6114984edbd1a70ecf7c
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-22 01:41:51 +0100

    add example aggregate.sh call

--- benchmarks/inconsistent/family/benchmark_single.sh
+++ benchmarks/inconsistent/family/benchmark_single.sh
@@ -25,7 +25,7 @@ done
 echo $header
 
 # do benchmark
-echo -ne "$instance"
+echo -ne "$instance 1"	# add 1 in order to compute the total number of instances with a certain size
 
 # for all configurations
 i=0
@@ -36,12 +36,21 @@ do
 	linecount=$(cat $instance.numas|wc -l)
 	ret=$?
 	if [[ $ret == 0 ]]; then
-	        output=$(cat $instance.$i.time.dat)
+	        output="$(cat $instance.$i.time.dat) 0"	# no timeout --> add 0
 	else
-		output="---"
+		output="--- 1"	# timeout --> add 1
 	fi
 	echo -ne "$output"
-	echo -ne "$linecount"
+	echo -ne "$linecount"	# Two Options:
+				# 1. The good solution: call the aggregation script as follows:
+				#    cat *.out | ./aggregate 300 0 0 "3,6,9" "2,4,5,7,8,9" "" ""
+				# Columns 3,6,9 will be averaged (contain the runtime for the 3 configurations)
+				# Columns 2,4,5,6,7,8,9 are added (2: number of instances, 4,6,8: timeouts for the 3 configurations, 5,7,9: number of answersets for the 3 configurations)
+				# (Last two parameters say that we don't compute minimum or maximum of any columns)
+				# 2. Quick and dirty (if something does not work and time is pressing):
+				#    Add 0 as number of "timeouts" since the aggregation script will sum up every second column, i.e.,
+				#	echo -ne "$linecount 0"
+				#    (needs to be removed later from the final result)
 
 	rm $instance.$i.time.dat
 
