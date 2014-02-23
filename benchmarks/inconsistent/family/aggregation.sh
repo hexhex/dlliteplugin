3b77618d0d34b023c0029ddc85c8fa7be048e21c
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-23 13:46:46 +0100

    fix in run.sh

--- benchmarks/inconsistent/family/aggregation.sh
+++ benchmarks/inconsistent/family/aggregation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Call the default aggregation script as follows:
-#    aggregate 300 0 0 "3,6,9" "2,4,5,7,8,9" "" ""
-# Columns 3,6,9 will be averaged (contain the runtime for the 3 configurations)
-# Columns 2,4,5,7,8,10,11 are added (2: number of instances, 4,7,10: timeouts for the 3 configurations, 5,8,11: number of answersets for the 3 configurations)
+#    aggregate 300 0 0 "3,5,6,8,9,11" "2,4,7,10" "" ""
+# Columns 3,5,6,8,9,11 will be averaged (3,6,9: runtime for the 3 configurations, 5,8,11: number of answersets for the 3 configurations)
+# Columns 2,4,7,10 are added (2: number of instances, 4,7,10: timeouts for the 3 configurations)
 # (Last two parameters say that we don't compute minimum or maximum of any columns)
-aggregateresults.sh 300 0 0 "3,6,9" "2,4,5,7,8,10,11" "" ""
+aggregateresults.sh 300 0 0 "3,5,6,8,9,11" "2,4,7,10" "" ""
