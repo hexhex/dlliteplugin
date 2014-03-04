497becdf595e78b1f90c42aa3cf324b69d700fef
Christoph Redl <redl@kr.tuwien.ac.at>
2014-03-04 18:49:43 +0100

    fix bootstrap-extra.sh

--- bootstrap-extra.sh
+++ bootstrap-extra.sh
@@ -125,64 +125,65 @@ if [ $osx -eq 1 ]; then
 	cat $OWLCPPMAINDIR/libxml2-$LIBXML2V/autogen.sh | sed 's/libtoolize --version/echo 0/' > $OWLCPPMAINDIR/libxml2-$LIBXML2V/autogen.sh.bak
 	mv $OWLCPPMAINDIR/libxml2-$LIBXML2V/autogen.sh.bak $OWLCPPMAINDIR/libxml2-$LIBXML2V/autogen.sh
 fi
+cd ..
 
 echo "
 	echo \"Bootstrapping boost\"
-	cd owlcpp/boost_$BOOSTVU
+	cd owlcpp\\\\boost_$BOOSTVU
 	bootstrap.bat
-	cd ../..
+	cd ..\\\\..
 
 	echo \"Building owlcpp (including other necessary libraries\"
-	cd owlcpp/owlcpp-v$OWLCPPV
-	../boost_$BOOSTVU/tools/build/v2/b2.exe release
-	cd ../..
+	cd owlcpp\\\\owlcpp-v$OWLCPPV
+	../boost_$BOOSTVU\\\\tools\\\\build\\\\v2\\\\b2.exe release
+	cd ..\\\\..
 
 	echo \"Extracting files\"
 	cd owlcpp
 	mkdir libs
 
 	echo \"   owlcpp libs\"
-	copy owlcpp-v$OWLCPPV/out/bin/io/msvc-10.0/release/link-static/threading-multi/owlcpp_io.lib libs/
-	copy owlcpp-v$OWLCPPV/out/bin/logic/msvc-10.0/release/link-static/threading-multi/owlcpp_logic.lib libs/
-	copy owlcpp-v$OWLCPPV/out/bin/rdf/msvc-10.0/release/link-static/threading-multi/owlcpp_rdf.lib libs/
+	copy owlcpp-v$OWLCPPV\\\\out\\\\bin\\\\io\\\\msvc-10.0\\\\release\\\\link-static\\\\threading-multi\\\\owlcpp_io.lib libs\\\\
+	copy owlcpp-v$OWLCPPV\\\\out\\\\bin\\\\logic\\\\msvc-10.0\\\\release\\\\link-static\\\\threading-multi\\\\owlcpp_logic.lib libs\\\\
+	copy owlcpp-v$OWLCPPV\\\\out\\\\bin\\\\rdf\\\\msvc-10.0\\\\release\\\\link-static\\\\threading-multi\\\\owlcpp_rdf.lib libs\\\\
 	echo \"   factpp libs\"
-	copy owlcpp-v$OWLCPPV/out/ext/factpp/factpp/msvc-10.0/release/link-static/threading-multi/libfactpp_kernel-vc100-mt.lib libs/
+	copy owlcpp-v$OWLCPPV\\\\out\\\\ext\\\\factpp\\\\factpp\\\\msvc-10.0\\\\release\\\\link-static\\\\threading-multi\\\\libfactpp_kernel-vc100-mt.lib libs\\\\
 	echo \"   libxml2 libs\"
-	copy owlcpp-v$OWLCPPV/out/ext/libxml2/libxml2/msvc-10.0/release/libxml2-version-2.9.0/link-static/threading-multi/libxml2-vc100-mt-2_9.lib libs/
+	copy owlcpp-v$OWLCPPV\\\\out\\\\ext\\\\libxml2\\\\libxml2\\\\msvc-10.0\\\\release\\\\libxml2-version-2.9.0\\\\link-static\\\\threading-multi\\\\libxml2-vc100-mt-2_9.lib libs\\\\
 	echo \"   libraptor libs\"
-	copy owlcpp-v$OWLCPPV/out/ext/raptor/raptor/msvc-10.0/release/link-static/raptor-version-2.0.8/threading-multi/libraptor-vc100-mt-2_0.lib libs/
+	copy owlcpp-v$OWLCPPV\\\\out\\\\ext\\\\raptor\\\\raptor\\\\msvc-10.0\\\\release\\\\link-static\\\\raptor-version-2.0.8\\\\threading-multi\\\\libraptor-vc100-mt-2_0.lib libs\\\\
 
 	echo \"   owlcpp headers\"
 	mkdir include
-	mkdir include/owlcpp
-	mkdir include/owlcpp/detail
-	mkdir include/owlcpp/io
-	mkdir include/owlcpp/io/detail
-	mkdir include/owlcpp/logic
-	mkdir include/owlcpp/logic/detail
-	mkdir include/owlcpp/rdf
-	mkdir include/owlcpp/rdf/detail
-	mkdir include/owlcpp/terms
-	mkdir include/owlcpp/terms/detail
-	copy owlcpp-v$OWLCPPV/include/owlcpp/*.h include/owlcpp/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/detail/*.h include/owlcpp/detail/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/io/*.h include/owlcpp/io/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/io/detail/*.h include/owlcpp/io/detail/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/logic/*.h include/owlcpp/logic/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/logic/detail/*.h include/owlcpp/logic/detail/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/rdf/*.h include/owlcpp/rdf/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/rdf/detail/*.h include/owlcpp/rdf/detail/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/terms/*.h include/owlcpp/terms/
-	copy owlcpp-v$OWLCPPV/include/owlcpp/terms/detail/*.h include/owlcpp/terms/detail/
+	mkdir include\\\\owlcpp
+	mkdir include\\\\owlcpp\\\\detail
+	mkdir include\\\\owlcpp\\\\io
+	mkdir include\\\\owlcpp\\\\io\\\\detail
+	mkdir include\\\\owlcpp\\\\logic
+	mkdir include\\\\owlcpp\\\\logic\\\\detail
+	mkdir include\\\\owlcpp\\\\rdf
+	mkdir include\\\\owlcpp\\\\rdf\\\\detail
+	mkdir include\\\\owlcpp\\\\terms
+	mkdir include\\\\owlcpp\\\\terms\\\\detail
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\*.h include\\\\owlcpp\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\detail\\\\*.h include\\\\owlcpp\\\\detail\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\io\\\\*.h include\\\\owlcpp\\\\io\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\io\\\\detail\\\\*.h include\\\\owlcpp\\\\io\\\\detail\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\logic\\\\*.h include\\\\owlcpp\\\\logic\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\logic\\\\detail\\\\*.h include\\\\owlcpp\\\\logic\\\\detail\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\rdf\\\\*.h include\\\\owlcpp\\\\rdf\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\rdf\\\\detail\\\\*.h include\\\\owlcpp\\\\rdf\\\\detail\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\terms\\\\*.h include\\\\owlcpp\\\\terms\\\\
+	copy owlcpp-v$OWLCPPV\\\\include\\\\owlcpp\\\\terms\\\\detail\\\\*.h include\\\\owlcpp\\\\terms\\\\detail\\\\
 	echo \"   factpp headers\"
-	mkdir include/factpp
-	copy owlcpp-v$OWLCPPV/out/include/factpp/*.h include/owlcpp/factpp/
+	mkdir include\\\\factpp
+	copy owlcpp-v$OWLCPPV\\\\out\\\\include\\\\factpp\\\\*.h include\\\\owlcpp\\\\factpp\\\\
 	echo \"   libxml2 headers\"
-	mkdir include/libxml
-	copy owlcpp-v$OWLCPPV/out/include/libxml/*.h include/owlcpp/libxml/
+	mkdir include\\\\libxml
+	copy owlcpp-v$OWLCPPV\\\\out\\\\include\\\\libxml\\\\*.h include\\\\owlcpp\\\\libxml\\\\
 	echo \"   raptor headers\"
-	mkdir include/raptor
-	copy owlcpp-v$OWLCPPV/out/include/raptor/*.h include/owlcpp/raptor/
+	mkdir include\\\\raptor
+	copy owlcpp-v$OWLCPPV\\\\out\\\\include\\\\raptor\\\\*.h include\\\\owlcpp\\\\raptor\\\\
 	" > build_owlcpp.bat
 
 echo "Fixing jam-file"
