343f7555dd92f7ce1cbb56b8411179a13434d46f
dariastepanova <dasha@kr.tuwien.ac.at>
2014-10-31 18:21:03 +0100

    added taxi benchmark with time constraints

---
+++ benchmarks/inconsistent/DLLite/taxi/time/generate.sh
@@ -0,0 +1,50 @@
+# $1: instance size
+# $2: customer probability
+
+drivers=$(($1*20))
+regions=$(($1*5))
+customers=$(($1*50))
+
+#echo $drivers
+#echo $customers
+#echo $regions
+
+prob=$((32768 * $2 / 100)) 
+for (( i=1; i <= $customers; i++ ))
+do
+	if [[ $RANDOM -le $prob ]]; then 
+		#r=$((RANDOM%$regions+1))
+		echo "isIn(\"c$i\",\"r$((RANDOM%$regions+1))\")."
+		echo "needsTo(\"c$i\",\"r$((RANDOM%$regions+1))\", $((RANDOM%23)))."
+
+	fi
+done
+
+if [[ $2 -le 10 ]]; then 
+	prob1=$((32768 * 30 / 100)) 
+
+elif [[ $2 -le 20 ]]; then 
+	prob1=$((32768 * 70 / 100)) 
+
+else 
+	prob1=32768 
+
+fi 
+
+prob2=$((32768 * 50 / 100)) 
+
+for (( i=1; i <= $drivers; i++ ))
+do
+	if [[ $RANDOM -le $prob2 ]]; then
+		r=$((RANDOM%$regions+1))
+		echo "isIn(\"d$i\",\"r$r\")."
+
+	for (( j=1; j<=$regions; j++ ))
+	do
+		if [[ $RANDOM -le $prob2 ]]; then
+			echo "goTo(\"d$i\",\"r$j\")."	
+		fi	
+	done
+
+	fi			
+done	
---
+++ benchmarks/inconsistent/DLLite/taxi/time/generateset.sh
@@ -0,0 +1,47 @@
+# $1 starting probabilty
+# $2 finishing probability
+# $3 step
+# $4 number of instances
+# $5 size parameter: for n: n*20 drivers, n*5 regions, n*50 customers 
+
+
+if [[ $# -lt 4 ]]; then
+        echo "Error: Script expects 4 parameters
+		1: starting probability 
+		2: finishing probability
+		3: step	
+		4: number of instances
+		5: instance size"
+        exit 1;
+fi
+
+
+# create a directory for storing benchmark instances
+
+if [ -d "instances" ]; then
+	rm instances/*.*
+else 
+	mkdir -p instances
+fi
+
+./generate_ontology.sh $5 > ontology.owl
+
+
+for (( prob=$1; prob <= $2; prob+=$3 ))
+do
+	for (( inst=0; inst < $4; inst++ ))
+	do
+		probf=`printf "%03d" ${prob}`
+		in=`printf "%03d" ${inst}`
+
+		# create ontology instances
+		cp ontology.owl instances/inst_size_${probf}_inst_${in}.owl
+
+		# instantiate the program
+      		./generate.sh $5 $prob > "instances/inst_size_${probf}_inst_${in}.hex"
+
+
+		cat program.hex | sed "s/OWLONTOLOGY/\"instances\/inst_size_${probf}_inst_${in}.owl\"/g" >> "instances/inst_size_${probf}_inst_${in}.hex"
+	
+	done
+done
