9ed629dacc26e3aabb26a7cd970a82c9768b7cb1
dariastepanova <dasha@kr.tuwien.ac.at>
2014-11-25 17:16:01 +0100

    tuned the benchmark generati script

--- benchmarks/inconsistent/DLLite/taxi/districts/generate.sh
+++ benchmarks/inconsistent/DLLite/taxi/districts/generate.sh
@@ -5,32 +5,27 @@ drivers=$(($1*20))
 regions=23
 customers=$(($1*50))
 
-#echo $drivers
-#echo $customers
-#echo $regions
 
-prob=$((32768 * $2 / 100)) 
+probcust=$((32768 * $2 / 100)) 
 for (( i=1; i <= $customers; i++ ))
 do
-	if [[ $RANDOM -le $prob ]]; then 
-		#r=$((RANDOM%$regions+1))
+	if [[ $RANDOM -le $probcust ]]; then 
 		echo "isIn(\"c$i\",\"r$((RANDOM%$regions+1))\")."
 		echo "needsTo(\"c$i\",\"r$((RANDOM%$regions+1))\")."
-
 	fi
 done
 
-prob2=$((32768 * 50 / 100)) 
+probdriv=$(((probcust / 3)+4))
 
 for (( i=1; i <= $drivers; i++ ))
 do
-	if [[ $RANDOM -le $prob2 ]]; then
+	if [[ $RANDOM -le $probdriv ]]; then
 		r=$((RANDOM%$regions+1))
 		echo "isIn(\"d$i\",\"r$r\")."
 
 	for (( j=1; j<=$regions; j++ ))
 	do
-		if [[ $RANDOM -le $prob2 ]]; then
+		if [[ $RANDOM -le $((probdriv*2)) ]]; then
 			echo "goTo(\"d$i\",\"r$j\")."	
 		fi	
 	done
